775b03ed5cd26a414e8e8c68c65f238b1bb06cd3
David Kissinger <davidgkissinger@gmail.com>
2024-03-24 16:36:05 -0700

    Update getvars.php

--- getvars.php
+++ getvars.php
@@ -4,13 +4,16 @@
  * This code fits into functions.php or a plug-in.
  * Best for text only - arrays, images etc not supported
  *
+ * Ref: https://developer.wordpress.org/reference/functions/get_post_field
+ *
  * @license GPL 3.0
  */
 function dk_get_var( $atts ) {
   // attribute defaults to title so that is returned it no atts are given
   global $post;
   $a = shortcode_atts( array(
-		'fieldname' => 'title',
+		'fieldname' => 'post_title',
 	 ), $atts );
+  return get_post_field( $a['fieldname'] );
 }
 add_shortcode( 'getpostvar', 'dk_get_var' );
