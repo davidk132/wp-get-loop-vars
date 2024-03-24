9ba657fc317a23518c199302f5e26436b92f8235
David Kissinger <davidgkissinger@gmail.com>
2024-03-24 16:02:10 -0700

    Update getvars.php

--- getvars.php
+++ getvars.php
@@ -1,1 +1,12 @@
-
+<?php
+/**
+ * Simple shortcode to grab values from WP query object. Must be in the loop or this won't work.
+ * This code fits into functions.php or a plug-in
+ * 
+ *
+ * @license GPL 3.0
+ */
+function get_var() {
+  // Do stuff
+}
+add_shortcode( 'getpostvar', 'get_var' );
