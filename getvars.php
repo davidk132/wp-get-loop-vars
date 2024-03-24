a5b19915bfc5598dbc27461c1a98f8829f8805cc
David Kissinger <davidgkissinger@gmail.com>
2024-03-24 16:10:22 -0700

    Update getvars.php

--- getvars.php
+++ getvars.php
@@ -1,12 +1,16 @@
 <?php
 /**
  * Simple shortcode to grab values from WP query object. Must be in the loop or this won't work.
- * This code fits into functions.php or a plug-in
- * 
+ * This code fits into functions.php or a plug-in.
+ * Best for text only - arrays, images etc not supported
  *
  * @license GPL 3.0
  */
-function get_var() {
-  // Do stuff
+function dk_get_var( $atts ) {
+  // attribute defaults to title so that is returned it no atts are given
+  global $post;
+  $a = shortcode_atts( array(
+		'fieldname' => 'title',
+	 ), $atts );
 }
-add_shortcode( 'getpostvar', 'get_var' );
+add_shortcode( 'getpostvar', 'dk_get_var' );
